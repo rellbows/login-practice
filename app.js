2b8e6f949c7c424db6eeacfa0200ebf3a4254d2d
rellbows <ryan.ellis927@gmail.com>
2020-04-13 00:36:08 -0500

    setup login form w/ styling

--- app.js
+++ app.js
@@ -2,19 +2,26 @@
 // description: simple login page in node/express utilizing passport and bcrypt
 // libraries.
 
+// basic modules
+var path = require('path');
+
+// importing basic modules...
 var express = require('express');
 var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
 
-var login = require('./login.js');
+// basic config stuff..
+app.engine('handlebars', handlebars.engine);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, '/public'))); // where static .js files are served
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
 
-app.use('/login', things);
+app.use('/login', require('./login.js'));
 
 app.get('/', function(req, res){
 	res.send("Hello World!");
-});
-
-app.get('/:id', function(req, res){
-	res.send('The id you specified was ' + req.params.id);
 });
 
 // invalid URL
@@ -22,4 +29,6 @@ app.get('*', function(req, res){
 	res.send('Sorry, this is an invalid	URL.');
 });
 
-app.listen(3000);
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:' + app.get('port') + '; Press Ctrl-C to terminate.');
+});
--- login.js
+++ login.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res){
+	res.render('login.handlebars');
+});
 
+router.post('/', function(req, res){
+	
+})
 
 // export router to use in app.js
 module.exports = router;
