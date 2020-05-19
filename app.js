7f81defa5f839a3e918df4e5fed7d16a18e7db72
rellbows <ryan.ellis927@gmail.com>
2020-05-18 19:17:16 -0500

    Basic login portal setup using sessions

--- app.js
+++ app.js
@@ -2,14 +2,21 @@
 // description: simple login page in node/express utilizing passport and bcrypt
 // libraries.
 
-// basic modules
+// basic modules needed
 var path = require('path');
-
-// importing basic modules...
 var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var mysql = require('./dbcon.js');
+var session = require('express-session');
 var bodyParser = require('body-parser');
+
+// setup session
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitiated: true
+}));
 
 // basic config stuff..
 app.engine('handlebars', handlebars.engine);
@@ -17,11 +24,17 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, '/public'))); // where static .js files are served
 app.set('view engine', 'handlebars');
 app.set('port', 3000);
+app.set('mysql', mysql);
 
 app.use('/login', require('./login.js'));
 
 app.get('/', function(req, res){
-	res.send("Hello World!");
+	if(req.session.loggedin){
+		res.send('Welcome back, ' + req.session.username + '!');
+	} else{
+		res.send('Please login to view this page!');
+	}
+	res.end();
 });
 
 // invalid URL
--- login.js
+++ login.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('./dbcon.js')
 
 router.get('/', function(req, res){
 	res.render('login.handlebars');
-});
+})
 
 router.post('/', function(req, res){
-	
-})
+	var username = req.body.username;
+	var password = req.body.password;
+	if(username && password){
+		mysql.pool.query('SELECT * FROM accounts WHERE username = ? and password = ?', [username, password], function(error, results, fields){
+			if(results.length > 0){
+				req.session.loggedin = true;
+				req.session.username = username;
+				res.redirect('..');
+			} else{
+				res.send('Incorrect Username and/or Password!');
+			}
+			res.end();
+		});
+	} else{
+		res.send('Please enter Username and Password!');
+		res.end();
+	}
+});
 
 // export router to use in app.js
 module.exports = router;
